4a4da573ba2d5e668ab0a7b1439e1f4148c54e4d
OvejaBasualdo <elportenio@gmail.com>
2023-02-10 18:07:12 -0300

    first commit

---
+++ src/main/java/com/microservices/paymentmodule/apiagateway/apiagateway/ApiagatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.paymentmodule.apiagateway.apiagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiagatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiagatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/paymentmodule/apiagateway/apiagateway/ApiagatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package com.microservices.paymentmodule.apiagateway.apiagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootTest
+class ApiagatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
